0d00cf37597eef580ec670885275fc7181d2ad5f
ibra <amerhanov_ibragim95@mail.ru>
2023-07-13 03:27:47 +0300

    as

--- src/test/java/test/service/CalculateServiceParamTest.java
+++ src/test/java/test/service/CalculateServiceParamTest.java
@@ -9,7 +9,7 @@ import test.exeptions.InCorrectArgumentsExeptions;
 public class CalculateServiceParamTest {
     public final CalculateService calculateService = new CalculateServiceImpl();
     @ParameterizedTest
-    @CsvSource({"8, 4, 4", "2, 1, 1", "4, 4, 0"})
+    @CsvSource({"8, 4, 12", "2, 1, 3", "4, 4, 8"})
     public void shouldReturnResult5WithPlus3and5(int num1, int num2, int expected) {
         int result = calculateService.sum(num1, num2);
         Assertions.assertEquals(result, expected);
@@ -21,15 +21,17 @@ public class CalculateServiceParamTest {
         Assertions.assertEquals(result, expected);
     }
     @ParameterizedTest
-    @CsvSource({"8, 4, 4", "2, 1, 1", "4, 4, 0"})
+    @CsvSource({"8, 4, 32", "2, 1, 2", "4, 4, 16"})
     public void shouldReturnResult4WithMultiplay2and2(int num1, int num2, int expected) {
         int result = calculateService.multiplay(num1, num2);
         Assertions.assertEquals(result, expected);
     }
     @ParameterizedTest
-    @CsvSource({"8, 4, 4", "2, 1, 1", "4, 4, 0"})
+    @CsvSource({"8, 4, 2", "2, 1, 2", "4, 4, 1"})
     public void shouldReturnResult7WithDevide70and10(int num1, int num2, int expected) throws InCorrectArgumentsExeptions {
         int result = calculateService.devide(num1, num2);
-        Assertions.assertEquals(result, expected);
+        if (num1 != 0) {
+            Assertions.assertEquals(result, expected);
+        }
     }
 }
